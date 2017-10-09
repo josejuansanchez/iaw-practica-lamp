87b0ea3e67fd03336616ee21ae53fa4c46462be3
José Juan Sánchez <josejuan.sanchez@gmail.com>
2017-10-09 14:06:18 +0200

    Updated database.sql

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS `practica01` DEFAULT CHARACTER SET utf8 COLLATE ut
 USE `practica01`;
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL auto_increment,
   `name` varchar(100) NOT NULL,
   `age` int(3) NOT NULL,
   `email` varchar(100) NOT NULL
