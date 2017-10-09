a3d9989238620a1677455c7a0dfb4e311e21f747
José Juan Sánchez <josejuan.sanchez@gmail.com>
2017-10-09 14:11:55 +0200

    Updated database.sql

--- database.sql
+++ database.sql
@@ -6,6 +6,7 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL auto_increment,
   `name` varchar(100) NOT NULL,
   `age` int(3) NOT NULL,
-  `email` varchar(100) NOT NULL
+  `email` varchar(100) NOT NULL,
+   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
