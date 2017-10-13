70d301e2576817ac2004ea95d3f87982a5f0eada
José Juan Sánchez <josejuan.sanchez@gmail.com>
2017-10-13 17:46:57 +0200

    Fixed minor issue

--- src/edit.php
+++ src/edit.php
@@ -6,22 +6,22 @@ if(isset($_POST['update'])) {
 	$id = mysqli_real_escape_string($mysqli, $_POST['id']);
 	$name = mysqli_real_escape_string($mysqli, $_POST['name']);
 	$age = mysqli_real_escape_string($mysqli, $_POST['age']);
-	$email = mysqli_real_escape_string($mysqli, $_POST['email']);	
-	
+	$email = mysqli_real_escape_string($mysqli, $_POST['email']);
+
 	// checking empty fields
-	if(empty($name) || empty($age) || empty($email)) {	
+	if(empty($name) || empty($age) || empty($email)) {
 		if(empty($name)) {
 			echo "<font color='red'>Name field is empty.</font><br/>";
 		}
-		
+
 		if(empty($age)) {
 			echo "<font color='red'>Age field is empty.</font><br/>";
 		}
-		
+
 		if(empty($email)) {
 			echo "<font color='red'>Email field is empty.</font><br/>";
-		}		
-	} else {	
+		}
+	} else {
 		// updating the table
 		$stmt = mysqli_prepare($mysqli, "UPDATE users SET name=?,age=?,email=? WHERE id=?");
 		mysqli_stmt_bind_param($stmt, "sisi", $name, $age, $email, $id);
@@ -38,39 +38,35 @@ if(isset($_POST['update'])) {
 $id = $_GET['id'];
 
 // selecting data associated with this particular id
-$stmt = mysqli_prepare($mysqli, "SELECT * FROM users WHERE id=?");
+$stmt = mysqli_prepare($mysqli, "SELECT name, age, email FROM users WHERE id=?");
 mysqli_stmt_bind_param($stmt, "i", $id);
 mysqli_stmt_execute($stmt);
-
-while($res = mysqli_fetch_array($result)) {
-	$name = $res['name'];
-	$age = $res['age'];
-	$email = $res['email'];
-}
+mysqli_stmt_bind_result($stmt, $name, $age, $email);
+mysqli_stmt_fetch($stmt);
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>	
-	<meta charset="UTF-8">	
+<head>
+	<meta charset="UTF-8">
 	<title>Edit Data</title>
 </head>
 
 <body>
 	<a href="index.php">Home</a>
 	<br/><br/>
-	
+
 	<form name="form1" method="post" action="edit.php">
 		<table border="0">
-			<tr> 
+			<tr>
 				<td>Name</td>
 				<td><input type="text" name="name" value="<?php echo $name;?>"></td>
 			</tr>
-			<tr> 
+			<tr>
 				<td>Age</td>
 				<td><input type="text" name="age" value="<?php echo $age;?>"></td>
 			</tr>
-			<tr> 
+			<tr>
 				<td>Email</td>
 				<td><input type="text" name="email" value="<?php echo $email;?>"></td>
 			</tr>
