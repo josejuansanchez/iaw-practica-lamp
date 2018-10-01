a3815e2e6e894659636661b152b5b80e8bdaa81d
José Juan Sánchez <josejuan.sanchez@gmail.com>
2018-10-01 11:32:05 +0000

    Added new lines

--- src/index.php
+++ src/index.php
@@ -26,11 +26,12 @@ $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
 
 	<?php
 	while($res = mysqli_fetch_array($result)) {
-		echo "<tr>";
-		echo "<td>".$res['name']."</td>";
-		echo "<td>".$res['age']."</td>";
-		echo "<td>".$res['email']."</td>";
-		echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
+		echo "<tr>\n";
+		echo "<td>".$res['name']."</td>\n";
+		echo "<td>".$res['age']."</td>\n";
+		echo "<td>".$res['email']."</td>\n";
+		echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>\n";
+		echo "</tr>\n";
 	}
 
 	mysqli_close($msqli);
