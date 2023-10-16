5a1b38ecc4bff591bdfa9287a52634a50ed47e41
José Juan Sánchez <josejuan.sanchez@gmail.com>
2023-10-17 01:21:53 +0200

    Se añaden comentarios al script .sql y se modifican las variables de config.php

--- db/database.sql
+++ db/database.sql
@@ -1,17 +1,21 @@
+-- Create a database
 DROP DATABASE IF EXISTS lamp_db;
 CREATE DATABASE lamp_db CHARSET utf8mb4;
 USE lamp_db;
 
+-- Create the users table
 CREATE TABLE users (
-  id int(11) NOT NULL auto_increment,
-  name varchar(100) NOT NULL,
-  age int(3) NOT NULL,
-  email varchar(100) NOT NULL,
-  PRIMARY KEY (id)
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  age INT UNSIGNED NOT NULL,
+  email VARCHAR(100) UNIQUE NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*
-CREATE USER IF NOT EXISTS 'lamp_user'@'%';
-SET PASSWORD FOR 'lamp_user'@'%' = 'lamp_password';
-GRANT ALL PRIVILEGES ON lamp_db.* TO 'lamp_user'@'%';
+-- Example about how to create a new database user and grant privileges.
+-- Remember to change the static values of: DB_USER, DB_PASSWORD and DB_NAME
+
+DROP USER IF EXISTS 'DB_USER'@'%';
+CREATE USER 'DB_USER'@'%' IDENTIFIED BY 'DB_PASSWORD';
+GRANT ALL PRIVILEGES ON DB_NAME.* TO 'DB_USER'@'%';
 */
--- src/config.php
+++ src/config.php
@@ -1,9 +1,9 @@
 <?php
 
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'lamp_db');
-define('DB_USER', 'lamp_user');
-define('DB_PASSWORD', 'lamp_password');
+define('DB_NAME', 'database_name_here');
+define('DB_USER', 'username_here');
+define('DB_PASSWORD', 'password_here');
 
 $mysqli = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
